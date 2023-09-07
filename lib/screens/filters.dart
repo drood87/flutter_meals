99e0d588fa16b3331c6eef3c72ba2c2f7fbef001
Daniel Bernstein <d.bernstein@gothaer-digital.de>
2023-09-07 09:57:33 +0200

    [S8_L169]: adding a filters screen with switches

---
+++ lib/screens/filters.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({super.key});
+
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _isGlutenFree = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Filters'),
+      ),
+      body: Column(
+        children: [
+          SwitchListTile(
+            value: _isGlutenFree,
+            onChanged: (isChecked) {
+              setState(() {
+                _isGlutenFree = isChecked;
+              });
+            },
+            title: Text(
+              'Gluten-Free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              'Only include gluten-free meals',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(
+              left: 34,
+              right: 22,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
