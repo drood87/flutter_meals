c7b5a5cfee771fd6c789b48013bc2e1cdcd660d6
Daniel Bernstein <d.bernstein@gothaer-digital.de>
2023-10-04 14:00:26 +0200

    adding riverpod provider for dummy meals

---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/models/meal.dart';
+
+// StateNotifierProvider is optimized for data that changes
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+
+  void toggleMealFavoriteStatus(Meal meal) {
+    final isMealFavorite = state.contains(meal);
+
+    if (isMealFavorite) {
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      state = [...state, meal];
+    }
+  }
+}
+
+final favoriteMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
+  return FavoriteMealsNotifier();
+});
--- lib/providers/meals_provider.dart
+++ lib/providers/meals_provider.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals/data/dummy_data.dart';
 
+// Provider is commonly used for data that is static and does not change on runtime
+
 final mealsProvider = Provider((ref) {
   return dummyMeals;
 });
