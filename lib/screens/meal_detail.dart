43f2a50d70465e85f238a4b3c3959f1680d3779a
Daniel Bernstein <d.bernstein@gothaer-digital.de>
2023-10-06 12:03:33 +0200

    [S10_199]: animation config

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -38,12 +38,13 @@ class MealDetailsScreen extends ConsumerWidget {
               duration: const Duration(milliseconds: 300),
               transitionBuilder: (child, animation) {
                 return RotationTransition(
-                  turns: animation,
+                  turns: Tween<double>(begin: .8, end: 1).animate(animation),
                   child: child,
                 );
               },
               child: Icon(
                 isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
               ),
             ),
           ),
